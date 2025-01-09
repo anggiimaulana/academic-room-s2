0dede1e14492053c5d1e312cff43ac46c93505c7
anggii <anggabos120@gmail.com>
2025-01-10 02:37:40 +0700

    ARs2 - Form Handling 1 file

---
+++ Form Handling/gabung/post.php
@@ -0,0 +1,36 @@
+<?php
+if (isset($_POST['submit'])) {
+    // Menangkap data form
+    $nama = $_POST['nama'];
+    $email = $_POST['email'];
+
+    // Validasi sederhana
+    if (empty($nama) || empty($email)) {
+        echo "<p style='color:red;'>Nama dan Email wajib diisi.</p>";
+    } else {
+        echo "<p style='color:green;'>Data berhasil dikirim!</p>";
+        echo "<p>Nama: $nama</p>";
+        echo "<p>Email: $email</p>";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form Sederhana</title>
+</head>
+<body>
+    <form method="POST" action="">
+        <label for="nama">Nama:</label>
+        <input type="text" id="nama" name="nama"><br><br>
+
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email"><br><br>
+
+        <button type="submit" name="submit">Kirim</button>
+    </form>
+</body>
+</html>
